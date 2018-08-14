f89b3ba943dbc08d048c60e1dd53d7c57e73d9df
pedrocunial <pedrocunial@gmail.com>
2018-08-14 09:12:22 -0300

    wip -- aula 2

---
+++ aula02/niosHello/niosHello_inst.vhd
@@ -0,0 +1,15 @@
+	component niosHello is
+		port (
+			clk_clk       : in  std_logic                    := 'X'; -- clk
+			reset_reset_n : in  std_logic                    := 'X'; -- reset_n
+			leds_export   : out std_logic_vector(5 downto 0)         -- export
+		);
+	end component niosHello;
+
+	u0 : component niosHello
+		port map (
+			clk_clk       => CONNECTED_TO_clk_clk,       --   clk.clk
+			reset_reset_n => CONNECTED_TO_reset_reset_n, -- reset.reset_n
+			leds_export   => CONNECTED_TO_leds_export    --  leds.export
+		);
+
