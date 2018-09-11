e023a2f8d5c46566c4e8c6f59463117539c81067
pedrocunial <pedrocc4@al.insper.edu.br>
2018-09-11 08:14:24 -0300

    wip -- ninios aula02

---
+++ aula02/software/aula02/obj/default/hello_world.d
@@ -0,0 +1,13 @@
+obj/default/hello_world.o: hello_world.c ../aula02_bsp/system.h \
+ ../aula02_bsp/linker.h ../aula02_bsp//HAL/inc/alt_types.h \
+ ../aula02_bsp//HAL/inc/io.h ../aula02_bsp//HAL/inc/alt_types.h
+
+../aula02_bsp/system.h:
+
+../aula02_bsp/linker.h:
+
+../aula02_bsp//HAL/inc/alt_types.h:
+
+../aula02_bsp//HAL/inc/io.h:
+
+../aula02_bsp//HAL/inc/alt_types.h:
